0c34d966c146337d9f735e6c297966bf748e8d1b
xiuqiang1995 <a18805819821@gmail.com>
2019-08-13 11:23:19 +0800

    github deploy

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="wrapper">
+  <div>第 {{n}} 手</div>
+  <div class="chess">
+  <div class="row">
+    <Cell @click="onClickCell(0,$event)" :n="n"/>
+    <Cell @click="onClickCell(1,$event)" :n="n"/>
+    <Cell @click="onClickCell(2,$event)" :n="n"/>
   </div>
+  <div class="row">
+    <Cell @click="onClickCell(3,$event)" :n="n"/>
+    <Cell @click="onClickCell(4,$event)" :n="n"/>
+    <Cell @click="onClickCell(5,$event)" :n="n"/>
+  </div>
+  <div class="row">
+    <Cell @click="onClickCell(6,$event)" :n="n"/>
+    <Cell @click="onClickCell(7,$event)" :n="n"/>
+    <Cell @click="onClickCell(8,$event)" :n="n"/>
+  </div>
+  </div>
+  <div>{{result}}</div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cell from "./Cell";
+export default{
+  components:{Cell},
+  data(){
+    return{
+    n:0,
+    map:[
+      [null,null,null],
+      [null,null,null],
+      [null,null,null]
+    ],
+    result:'比赛进行中...'
+  };
+  },
+  methods:{
+    onClickCell(i,text){
+      console.log(`第${i}个cell被点了,内容是:${text}`)
+      this.map[Math.floor(i/3)][i%3]= text;
+      this.n++;
+      this.tell()
+    },
+    tell(){
+      const map = this.map
+      //横
+      for(let i=0;i<2;i++){
+        if(map[i][0] !== null && map[i][0] === map[i][1] && map[i][1] === map[i][2]){
+          this.result =  map[i][0]+' 获胜了';
+        }
+      }
+      //竖
+      for(let j=0;j<2;j++){
+        if(map[0][j] !== null && 
+           map[0][j] === map[1][j] && 
+           map[1][j] === map[2][j]){
+          this.result =  map[0][j]+' 获胜了';
+        }
+      }
+      //对角线
+      if(map[0][0] !== null && map[0][0] === map[1][1] && map[1][1] === map[2][2]){
+        this.result = map[0][0]+' 获胜了';
+      }
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+      //对角线
+      if(map[0][2] !== null && map[0][2] === map[1][1] && map[1][1] === map[2][0]){
+        this.result = map[0][2]+' 获胜了';
+      }
+
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.row{
+  display:flex
+}
+.wrapper{
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    flex-direction:column;
 }
 </style>
---
+++ src/Cell.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class='cell' v-on:click="onClickSelf">
+      <template v-if="a">
+        {{text}}
+      </template>
+      <template v-else > 
+      </template>
+    </div>
+    </div>
+</template>
+
+<script>
+export default{
+  props:["n"],
+  data(){
+    return{a:false,text:""}
+  },
+  methods:{
+    onClickSelf(){
+      if(this.text !== ""){
+        return;
+      }
+      this.a =  true;
+      //判断是 x 还是 o
+      this.text = this.n %2 === 0?"x":"o";
+      //告诉外界 我被点了,并把x或o传出去
+      this.$emit("click",this.text);
+  }
+}
+};
+</script>
+
+
+<style>
+.cell{
+  border:1px solid black;
+  width:100px;
+  height:100px;
+
+  /* 文字居中 */
+  display:flex;
+  justify-content:center;
+  align-items:center;
+
+  font-size:80px;
+}
+</style>
